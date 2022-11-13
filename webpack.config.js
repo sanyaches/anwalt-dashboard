f92cbf2f1ef3e1314b5504a55c0c5ac67b2edb40
aleksandr.savkin <aleksandr.savkin@dicehub.com>
2022-11-13 22:14:31 +0300

    Fix alias in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader')
+const webpack = require('webpack')
 
 module.exports = {
   entry: './src/main.ts',
@@ -8,7 +9,17 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js',
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    extensions: ['.mjs', '.js', '.ts', '.tsx', '.vue', '.json'],
+  },
   plugins: [
+    new webpack.DefinePlugin({
+      __VUE_PROD_DEVTOOLS__: JSON.stringify('production') ? false : true,
+      __VUE_OPTIONS_API__: true
+    }),
     new HtmlWebpackPlugin({
       title: 'Anwalt.de dashboard',
       template: path.resolve(__dirname, './public/index.html'),
